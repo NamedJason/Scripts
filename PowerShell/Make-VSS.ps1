dd6e2a6d94f9215fc7396484c0a7adf4904b443b
NamedJason <github.jason@gmail.com>
2015-10-14 09:24:46 -0700

    Makes Standard Switches on an ESXi Host

---
+++ PowerShell/Make-VSS.ps1
@@ -0,0 +1,36 @@
+#Copies all Port Groups from a Distributed vSwitch to a Standard vSwitch
+#Author: Jason Coleman (virtuallyjason.blogspot.com)
+#Usage: Make-VSS.ps1 -h [the target ESXi host to get the new standard switch, must have access to the DVS] -s [the name of the DVS to copy] -d [the name of the VSS to copy the Port Groups to]
+
+param
+(
+	[alias("h")]
+	[string]$thisHost = $(read-host -Prompt "Enter the target Host"),
+	[alias("s")]
+	[string]$source = $(read-host -Prompt "Enter the Source Distributed Virtual Switch name"),
+	[alias("d")]
+	[string]$destination = $(read-host -Prompt "Enter the Destination Virtual Switch name"),
+	[alias("o")]
+	[string]$outputFile = "C:\Temp\PGTranslations.xml"
+)
+#Create an empty array to store the port group translations
+$pgTranslations = @()
+#Get the destination vSwitch
+$destSwitch = Get-VirtualSwitch -host $thisHost -name $destination
+#Get a list of all port groups on the source distributed vSwitch
+$allPGs = get-vdswitch -name $source | get-vdportgroup
+foreach ($thisPG in $allPGs)
+{
+	$thisObj = new-object -Type PSObject
+	$thisObj | add-member -MemberType NoteProperty -Name "dVSPG" -Value $thisPG.Name
+	$thisObj | add-member -MemberType NoteProperty -Name "VSSPG" -Value "$($thisPG.Name)-VSS"
+	new-virtualportgroup -virtualswitch $destSwitch -name "$($thisPG.Name)-VSS"
+	# Ensure that we don't try to tag an untagged VLAN
+	if ($thisPG.vlanconfiguration.vlanid)
+	{
+		get-virtualportgroup -virtualswitch $destSwitch -name "$($thisPG.Name)-VSS" | Set-VirtualPortGroup -vlanid $thisPG.vlanconfiguration.vlanid
+	}
+	$pgTranslations += $thisObj
+} 
+
+$pgTranslations | export-clixml $outputFile
