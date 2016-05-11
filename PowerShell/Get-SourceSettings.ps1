fda53aaba4e18fabbeff08ca635c06f25cd96263
NamedJason <named_jason@hotmail.com>
2016-05-11 13:54:02 -0700

    Fixed various issues with permissions

--- PowerShell/Get-SourceSettings.ps1
+++ PowerShell/Get-SourceSettings.ps1
@@ -2,6 +2,7 @@
 param
 (
 	$directory = $(read-host "Enter local output directory"),
+	$datacenter = $(read-host "Enter datacenter"),
 	[switch]$getTemplates
 )
 #Takes a VI Folder object and returns an array of strings that represents that folder's absolute path in the inventory
@@ -16,11 +17,10 @@ function get-folderpath
 	{
 		$folderArray = @()
 		$folderArray += $thisFolder
-		while ($folderArray[-1].parent.parent.id -notLike "Datacenter*")
+		while ($folderArray[-1].parent.parent.id -match "Folder")
 		{
 			$folderArray += $folderArray[-1].parent
 		}
-		# $folderArray = $folderArray[0..$folderArray.count-2]
 		[array]::Reverse($folderArray)
 		#convert the array of folders to an array of strings with just the folder names
 		$folderStrArray = @()
@@ -37,28 +37,58 @@ $directory = $directory.trim("\") #" This comment is to fix the gistit syntax hi
 new-item $directory -type directory -erroraction silentlycontinue
 
 #Get Roles
-get-virole | ? {$_.issystem -eq $false} | export-clixml $directory\roles.xml
+get-virole | ? {$_.issystem -eq $false} | export-clixml $directory\$($datacenter)-roles.xml
 
 #Get Permissions
-get-vipermission | export-clixml $directory\permissions.xml
+$allPermissions = @()
+$foundPermissions = get-vipermission
+$i = 0
+foreach ($thisPermission in $foundPermissions)
+{
+	write-progress -Activity "Getting permissions" -percentComplete ($i / $foundPermissions.count * 100)
+	$objPerm = "" | select entity,type,Role,Principal,Propagate,folderType
+	$objPerm.type = $thisPermission.entity.id.split("-")[0]
+	$objPerm.Role = $thisPermission.role
+	$objPerm.Principal = $thisPermission.Principal
+	$objPerm.Propagate = $thisPermission.propagate
+	#Create an absolute path for a folder, otherwise store the name of the entity
+	if ($objPerm.type -eq "Folder")
+	{
+		$objPerm.entity = get-folderpath $thisPermission.entity
+		$objPerm.folderType = $thisPermission.entity.type
+	}
+	else
+	{
+		$objPerm.entity = $thisPermission.entity.name
+		$objPerm.folderType = ""
+	}
+	$allPermissions += $objPerm
+	$i++
+}
+$allPermissions | export-clixml $directory\$($datacenter)-permissions.xml
+
 
 #Get VM Folder Structure
 $outFolders = @()
-foreach ($thisFolder in (get-folder | ? {$_.type.tostring() -eq "VM" -and $_.parent.id -notLike "Datacenter*"}))
+$i = 0
+$foundFolders = get-datacenter $datacenter | get-folder | ? {$_.type.tostring() -eq "VM" -and $_.parent.id -notLike "Datacenter*"}
+foreach ($thisFolder in $foundFolders)
 {
+	write-progress -Activity "Getting VM folder structure" -percentComplete ($i / $foundFolders.count * 100)
 	$myFolder = "" | select path
 	$myFolder.path = get-folderpath $thisFolder
 	$outFolders += $myFolder
+	$i++
 }
-$outFolders | export-clixml $directory\folders.xml
+$outFolders | export-clixml $directory\$($datacenter)-folders.xml
 
 #Convert Templates to VMs (so that they can transition vCenters)
-get-template | select name | export-clixml $directory\Templates.xml
-if ($getTemplates){get-template | set-template -ToVM -confirm:$false}
+get-template | select name | export-clixml $directory\$($datacenter)-Templates.xml
+if ($getTemplates){get-datacenter $datacenter | get-template | set-template -ToVM -confirm:$false}
 
 #Get VM Locations
 $outVMs = @()
-$allVApps = get-vapp
+$allVApps = get-datacenter $datacenter | get-vapp
 $vAppVMs = $allVApps | get-vm
 if ($vAppVMs)
 {
@@ -74,14 +104,16 @@ if ($vAppVMs)
 		$myVApp.VMs = ($thisVApp | get-vm).name
 		$outVApps += $myVApp
 	}
-	$outVApps | export-clixml $directory\vApps.xml
+	$outVApps | export-clixml $directory\$($datacenter)-vApps.xml
 }
 else
 {
-	$allVMs = get-VM
+	$allVMs = get-datacenter $datacenter | get-VM
 }
+$i = 0
 foreach ($thisVM in $allVMs)
 {
+	write-progress -Activity "Getting VM locations" -percentComplete ($i / $allVMs.count * 100)
 	$myVM = "" | select name,folderPath
 	$myVM.name = $thisVM.name
 	if ($thisVM.folder.name -eq "VM")
@@ -93,5 +125,6 @@ foreach ($thisVM in $allVMs)
 		$myVM.folderPath = get-folderpath $thisVM.folder
 	}
 	$outVMs += $myVM
+	$i++
 }
-$outVMs | export-clixml $directory\VMs.xml
+$outVMs | export-clixml $directory\$($datacenter)-VMs.xml
