466980d4383b9e7e358e2f9ef6f9c5cdfc1f81ba
NamedJason <named_jason@hotmail.com>
2016-06-28 13:41:23 -0700

    Create Start-DownedVMs.ps1

---
+++ PowerShell/Start-DownedVMs.ps1
@@ -0,0 +1,19 @@
+#Discover which VMs were either Shut Down or Powered Off in the last day
+$allVMs = get-vm
+$targetVMs = @()
+foreach ($thisVM in $allVMs)
+{
+	$events = $thisVM | get-vievent -start ((get-date).adddays(-1).tostring().split(" ")[0])
+	if ($events.FullFormattedMessage -contains "Task: Initiate guest OS shutdown" -or $events.FullFormattedMessage -contains "Task: Power Off virtual machine")
+	{
+		$thisVM.name
+		$targetVMs += $thisVM
+	}
+}
+read-host "Press Enter to power on the discovered VMs..."
+
+#Power on the VMs...
+foreach ($thisVM in $targetVMs)
+{
+	if ($thisVM.powerstate -eq "poweredoff"){$thisVM | start-VM}
+}
