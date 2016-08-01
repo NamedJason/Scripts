9f1501e8bc88771ddd59a9c571f7cdd3c419b6d1
NamedJason <named_jason@hotmail.com>
2016-08-01 10:58:25 -0700

    Create group-VMs.ps1

---
+++ PowerShell/group-VMs.ps1
@@ -0,0 +1,49 @@
+#Detects and reports on any VMs that aren't part of any groups, then adds them to the specified Group
+#Must have the DRSRule module from PowerCLIGoodies (available on GitHub)
+#Requires -modules DRSRule
+param
+(
+	[parameter(parameterSetName='remediate',Mandatory=$true)]
+	[switch]$placeVMs,
+	[parameter(parameterSetName='report',Mandatory=$true)]
+	[switch]$report,
+	[parameter(Mandatory=$true)]
+	[string]$Cluster,
+	[parameter(parameterSetName='remediate',Mandatory=$true)]
+	[string]$GroupName
+)
+
+$clusterObj = get-cluster $cluster
+$groupedVMs = @()
+
+#Get a list of all VMs that are not part of any DRS VM Group
+write-host "Detecting DRS Cluster VM Groups..."
+$clusterObj | get-drsvmgroup | foreach {$groupedVMs += $_.vm}
+$groupedVMs = $groupedVMs | select -unique
+$allVMs = ($clusterObj | get-vm).name
+$ungroupedVMs = (compare-object $allVMs $GroupedVMs | ? {$_.SideIndicator -eq "<="}).InputObject
+
+if ($report)
+{
+	write-host "$('='*40)"
+	write-host "Machines that are not part of any group:"
+	write-host "$('='*40)"
+	$ungroupedVMs
+}
+	
+#Adds ungrouped VMs to the specfied group
+if ($placeVMs)
+{
+	if ($destinationGroup = $clusterObj | get-DrsVMGroup -name $groupName)
+	{
+		foreach ($thisVM in $ungroupedVMs)
+		{
+			write-host "Adding $thisVM to the $($destinationGroup.name) group."
+			$destinationGroup | Set-DrsVMGroup -vm (get-vm $thisVM) -Append > $NULL
+		}
+	}
+	else
+	{
+		write-error "Default group $groupName was not found on the $cluster cluster."
+	}
+}
