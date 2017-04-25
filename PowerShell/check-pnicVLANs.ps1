834fd3f8acf5d9e21ad4bfa0268f24e62fe52b1c
NamedJason <named_jason@hotmail.com>
2017-04-25 15:00:22 -0700

    Better output, easier to use for multiple switches
    The returned object array contains more information about each pnic discovered.  It also automatically discovers which VLANs are allowed on a specific uplink by looking at the Port Groups on the VDS that that uplink is attached to.  This makes it easier to run against multiple VDSwitches.

--- PowerShell/check-pnicVLANs.ps1
+++ PowerShell/check-pnicVLANs.ps1
@@ -1,39 +1,45 @@
 #check-pnicVLANs.ps1
-#Reports DVS PNICs with observed invalid VLAN traffic
-#usage: check-pnicVLANs.ps1 -vdswitch <switch name> -minVLANID <number> -maxVLANID <number>
+#Reports DVS PNICs with observed invalid VLAN traffic.  Uses the Port Groups on the VDS to determine what VLANs are valid, flags if it finds VLANs that are not represented by Port Groups.
+#usage: check-pnicVLANs.ps1 -vdswitch <switch name>
+#usage: $a = @(); get-vdswitch | % {$a += check-pnicVLANs.ps1 -vdswitch $_}
 #Examines the PNIC hints (using techniques from poshcode.org/1653) to find if any PNICs on a VDS observe VLANs that are outside of the specified acceptable range.
 param
 (
-	$vdswitch,
-	[int]$minVLANID = 0,
-	[int]$maxVLANID = 4096
+	$vdswitch
 )
-write-host "Getting ESXi hosts and Distributed Switch..."
+write-host "Getting ESXi hosts and Distributed Switch $vdswitch..."
 $allHosts = get-vmhost
 $vdswitch = get-vdswitch $vdswitch
+$portGroups = $vdswitch | get-vdportgroup
 $out = @()
+#Get the valid list of VLANs
+$validVLANs = $portGroups.vlanconfiguration.vlanid | sort | get-unique
+
+#Check the host pnics
 foreach ($vmhost in $vdswitch.ExtensionData.Config.Host){
-	$outObj = "" | select vmHost,MinVLANID,MaxVLANID
+	$outObj = "" | select vmHost,WrongVLANs,vDSwitch,validVLANs
 	$vmhostObj = $allHosts | ? {$_.id -like "*$($vmhost.config.host.value)"}
 	write-host "Working on $($vmhostObj.name)..."
 	$outObj.vmHost = $vmhostObj.name
-	$outObj.MinVLANID = $minVLANID
-	$outObj.MaxVLANID = $maxVLANID
+	$outObj.vDSwitch = $vdswitch.name
+	$outObj.validVLANs = $validVLANs -join(", ")
 	$hostView = $vmhostObj | Get-View -Property ConfigManager
 	$ns = Get-View $hostView.ConfigManager.NetworkSystem
 	foreach ($vmnic in $vmhost.config.backing.pnicspec.pnicdevice){
 		$vlans = $ns.QueryNetworkHint($vmnic).subnet.vlanid | sort
 		if ($vlans){
-			if ($vlans[0] -lt $minVLANID -or $vlans[-1] -gt $maxVLANID){
-			write-host "$($outObj.vmHost) $vmnic has out of range VLANs" -foregroundcolor red
+			if ($bad = (compare-object $validVLANs $vlans | ? {$_.sideIndicator -eq "=>"}).inputObject){
+				write-host "$($outObj.vmHost) $vmnic has out of range VLANs" -foregroundcolor red
+				$outObj.WrongVLANs += $bad
 			}
 		}
 		else
 		{
 			write-host "$($outObj.vmHost) $vmnic has no observed VLANs" -foregroundcolor yellow
 		}
-		$outObj | add-member -membertype NoteProperty -name $vmnic -value ($vlans -join(", "))
+		$outObj | add-member -membertype NoteProperty -name "$vmnic`_Observed" -value ($vlans -join(", "))
 	}
+	$outObj.WrongVLANs = $outObj.WrongVLANs -join(", ")
 	$out += $outObj
 }
 $out
